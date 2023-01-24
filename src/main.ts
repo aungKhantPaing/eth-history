ff007b5428c59e2a8d526b4c1f9890488c4c0bd5
akp <25960342+aungKhantPaing@users.noreply.github.com>
2023-01-24 21:38:40 +0630

    Add vuetify + mdi font

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,24 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
+import "@mdi/font/css/materialdesignicons.min.css";
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "dark",
+  },
+});
 
 const app = createApp(App);
 
-app.use(createPinia());
-app.use(router);
+app.use(createPinia()).use(router).use(vuetify);
 
 app.mount("#app");
